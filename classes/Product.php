548f3f2aa3f14415f06e188408664319027a4cee
Andria Begiashvili <92532950+AndriaBegiashvili@users.noreply.github.com>
2023-04-03 03:49:18 +0400

    Update Product.php

--- classes/Product.php
+++ classes/Product.php
@@ -2,22 +2,20 @@
 abstract class Product extends Dbconn implements ProductInterface{
     public function addProduct()
     {
-         public function addProduct()
-    {
         $sql = "SELECT sku FROM products WHERE sku like '$this->SKU'";
         $result = mysqli_query($this->connect(), $sql);
         if (count(mysqli_fetch_all($result, MYSQLI_ASSOC)) > 0){
             echo "<div>Product with the Given SKU already exists!</div>";
-        }
-        try {
+        }else{
+            try {
             $sql = "INSERT INTO products (SKU, Name, Price, Type, Value) VALUES ('$this->SKU', '$this->name', '$this->price', '$this->type', '$this->value')";
             $this->connect()->query($sql);
-            echo "Success";
-        } catch (\Throwable $th) {
+            header("location:index.php");
+         }catch (\Throwable $th) {
             echo $th;
         }
-    }
-    
+        } 
+        
     }
            
         
