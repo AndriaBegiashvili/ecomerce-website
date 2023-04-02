e61b8fff938f2fc534975383ca02dcb8cdc52c4f
Andria Begiashvili <92532950+AndriaBegiashvili@users.noreply.github.com>
2023-04-03 02:06:21 +0400

    Update Dbconn.php

--- classes/Dbconn.php
+++ classes/Dbconn.php
@@ -8,19 +8,14 @@ class Dbconn implements DbconnInterface {
 
     public function connect()
     {
-        // Create a new mysqli object
-        $mysqli = mysqli_connect($this->host, $this->username, $this->password, $this->dbname);
+       $mysqli = new mysqli($this->host, $this->username, $this->password, $this->dbname);
 
-        // Check if the connection was successful
-        if (!$mysqli) {
-            die("Connection failed: " . mysqli_connect_error());
+        if ($mysqli->connect_error) {
+            die("Connection failed: " . $mysqli->connect_error);
         }
+        $mysqli->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, true);
+        $mysqli->set_charset("utf8mb4");
 
-        // Set default fetch mode to associative array
-        mysqli_options($mysqli, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, true);
-        mysqli_set_charset($mysqli, "utf8mb4");
-
-        // Return the mysqli object
         return $mysqli;
     }
     public function delete($skus){
