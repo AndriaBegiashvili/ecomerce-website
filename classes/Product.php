246de35102f25c8bc7d1cad4799327a909ac7077
Andria Begiashvili <92532950+AndriaBegiashvili@users.noreply.github.com>
2023-04-03 03:15:10 +0400

    Update Product.php

--- classes/Product.php
+++ classes/Product.php
@@ -2,20 +2,22 @@
 abstract class Product extends Dbconn implements ProductInterface{
     public function addProduct()
     {
-         try {
-            $query = "INSERT INTO products (SKU, Name, Price, Type, Value) VALUES ('$this->SKU', '$this->name', '$this->price', '$this->type', '$this->value')";
-            $result = $this->connect()->query($query);
-            header('Location: index.php');
-
-        } catch (Exception $e) {
-            // Check if the error message indicates that the SKU already exists
-            if (strpos($e->getMessage(), "Duplicate entry") !== false) {
-                echo "<div>This SKU already exists</div>";
-            } else {
-                // Handle other exceptions here
-                echo "<div>An error occurred: " . $e->getMessage() . "</div>";
-            }
+         public function addProduct()
+    {
+        $sql = "SELECT sku FROM products WHERE sku like '$this->SKU'";
+        $result = mysqli_query($this->connect(), $sql);
+        if (count(mysqli_fetch_all($result, MYSQLI_ASSOC)) > 0){
+            echo "<div>Product with the Given SKU already exists!</div>";
         }
+        try {
+            $sql = "INSERT INTO products (SKU, Name, Price, Type, Value) VALUES ('$this->SKU', '$this->name', '$this->price', '$this->type', '$this->value')";
+            $this->connect()->query($sql);
+            echo "Success";
+        } catch (\Throwable $th) {
+            echo $th;
+        }
+    }
+    
     }
            
         
